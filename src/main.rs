b9b1e5d46659e5251fa2ab37c15013b57fc52a69
Jason Evans <jayevo95@gmail.com>
2026-02-12 00:54:18 +0000

    Converted game to 2D

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,6 @@ mod player;
 fn main() {
     let (mut rl, thread) = raylib::init().size(800, 600).title("Rogue Engine").build();
 
-    // Create camera
-    let camera = Camera3D::orthographic(
-        Vector3::new(0.0, 10.0, 10.0), // position
-        Vector3::new(0.0, 0.0, 0.0),   // target
-        Vector3::new(0.0, 1.0, 0.0),   // up
-        20.0,                          // fovy
-    );
-
     let mut player = Player::new(&mut rl, &thread);
 
     while !rl.window_should_close() {
@@ -25,9 +17,7 @@ fn main() {
 
         d.clear_background(Color::WHITE);
 
-        {
-            let mut d3d = d.begin_mode3D(camera);
-            player.draw(&mut d3d);
-        }
+        player.draw(&mut d);
+        
     }
 }
--- src/player.rs
+++ src/player.rs
@@ -1,56 +1,51 @@
 use raylib::prelude::*;
 
 pub struct Player {
-    position: Vector3,
-    model: Model,
-    speed: f32,
-    rotation_angle: f32,
+    idle_side_texture: Texture2D,
+    src_rect: Rectangle,
+    dest_rect: Rectangle,
+    position: Vector2,
 }
 
 impl Player {
     pub fn new(rl: &mut RaylibHandle, thread: &RaylibThread) -> Self {
-        let model = rl
-            .load_model(thread, "assets/Rogue.glb")
-            .expect("Failed to load player model");
+
+        let idle_side_texture = rl.load_texture(thread, "assets/player/Idle_base/Idle_Side-Sheet.png")
+        .expect("Failed to load player idle side texture");
 
         Player {
-            model,
-            position: Vector3::new(0.0, 0.0, 0.0),
-            speed: 5.0,
-            rotation_angle: 0.0,
+            idle_side_texture,
+            src_rect: Rectangle::new(0.0,0.0,64.0,64.0),
+            dest_rect: Rectangle::new(0.0,0.0,64.0,64.0),
+            position: Vector2::new(0.0,0.0),
         }
     }
 
-    pub fn draw(&mut self, d3d: &mut RaylibMode3D<RaylibDrawHandle>) {
-        d3d.draw_model_ex(
-            &self.model,
-            self.position,
-            Vector3::new(0.0, 1.0, 0.0),
-            self.rotation_angle,
-            Vector3::new(1.0, 1.0, 1.0),
+    pub fn draw(&mut self, d: &mut RaylibDrawHandle) {
+        d.draw_texture_pro(
+            &self.idle_side_texture,
+            self.src_rect,
+            self.dest_rect, Vector2::new(self.position.x,self.position.y),
+            0.0,
             Color::WHITE,
         );
     }
 
-    #[rustfmt::skip]
+
     pub fn update(&mut self, rl: &mut RaylibHandle, delta_time: f32) {
+
+        if rl.is_key_down(KeyboardKey::KEY_W) {
+            self.dest_rect.y -= 50.0 * delta_time;
+        }
         if rl.is_key_down(KeyboardKey::KEY_S) {
-
-            self.position.z += self.speed * delta_time;
-            self.rotation_angle = 0.0;
-
-        } else if rl.is_key_down(KeyboardKey::KEY_W) {
-
-            self.position.z -= self.speed * delta_time;
-            self.rotation_angle = -180.0;
-
-        } else if rl.is_key_down(KeyboardKey::KEY_D) {
-            self.position.x += self.speed * delta_time;
-            self.rotation_angle = 90.0;
-
-        } else if rl.is_key_down(KeyboardKey::KEY_A) {
-            self.position.x -= self.speed * delta_time;
-            self.rotation_angle = -90.0;
+            self.dest_rect.y += 50.0 * delta_time;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_D) {
+            self.dest_rect.x += 50.0 * delta_time;
+        }
+        if rl.is_key_down(KeyboardKey::KEY_A) {
+            self.dest_rect.x -= 50.0 * delta_time;
         }
     }
-}
+} 
+
